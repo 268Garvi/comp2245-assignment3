a56a7f8803aa8db34a52a188dd1bcaddd12acb94
Garvin Ponde <garvinponde@gmail.com>
2023-10-17 20:44:52 -0400

    Check for the winner and update the status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -3,11 +3,17 @@ document.addEventListener('DOMContentLoaded', function() {
     var squares = board.getElementsByTagName('div');
     var game = Array(squares.length).fill(null); // Initialize game state
     var turn = 'X'; // Start with player X
+    var gameWon = false;
   
     function updateGameState(index) {
-      if (game[index] === null) {
+      if (!gameWon && game[index] === null) {
         game[index] = turn;
-        switchTurn();
+        renderSquare(index);
+        if (checkWinner()) {
+          announceWinner();
+        } else {
+          switchTurn();
+        }
       }
     }
   
@@ -20,6 +26,30 @@ document.addEventListener('DOMContentLoaded', function() {
       turn = turn === 'X' ? 'O' : 'X';
     }
   
+    function checkWinner() {
+      const winCombinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6]             // Diagonals
+      ];
+  
+      for (const combination of winCombinations) {
+        const [a, b, c] = combination;
+        if (game[a] && game[a] === game[b] && game[a] === game[c]) {
+          gameWon = true;
+          return true;
+        }
+      }
+  
+      return false;
+    }
+  
+    function announceWinner() {
+        var status = document.getElementById('status');
+        status.textContent = `Congratulations! ${turn} is the Winner!`;
+        status.classList.add('you-won');
+      }
+      
     function handleSquareHover(index) {
       squares[index].classList.toggle('hover');
     }
@@ -29,9 +59,8 @@ document.addEventListener('DOMContentLoaded', function() {
       squares[i].addEventListener('click', function(e) {
         var index = Array.prototype.indexOf.call(squares, e.target);
         updateGameState(index);
-        renderSquare(index);
       });
-      
+  
       squares[i].addEventListener('mouseover', function(e) {
         var index = Array.prototype.indexOf.call(squares, e.target);
         handleSquareHover(index);
