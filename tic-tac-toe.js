dd4a3029ea9b839e9434f1c94b3a7ebef92abb55
Garvin Ponde <garvinponde@gmail.com>
2023-10-17 20:20:54 -0400

    Add X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,9 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
     var board = document.getElementById('board');
     var squares = board.getElementsByTagName('div');
+    var game = Array(squares.length).fill(null); // Initialize game state
+    var turn = 'X'; // Start with player X
   
-    for (var i = 0; i < squares.length; i++) {
+    function updateGameState(index) {
+      if (game[index] === null) {
+        game[index] = turn;
+        switchTurn();
+      }
+    }
+  
+    function renderSquare(index) {
+      squares[index].textContent = game[index];
+      squares[index].classList.add(game[index]);
+    }
+  
+    function switchTurn() {
+      turn = turn === 'X' ? 'O' : 'X';
+    }
+  
+    for (let i = 0; i < squares.length; i++) {
       squares[i].classList.add('square');
+      squares[i].addEventListener('click', function(e) {
+        var index = Array.prototype.indexOf.call(squares, e.target);
+        updateGameState(index);
+        renderSquare(index);
+      });
     }
   });
   
